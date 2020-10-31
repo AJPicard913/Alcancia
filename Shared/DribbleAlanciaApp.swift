e944c7be86407275a7fe5df993e092d36f9b352c
ajpicard913 <Contact@saucedesigns.io>
2020-10-30 22:19:54 -0400

    Initial Commit

---
+++ Shared/DribbleAlanciaApp.swift
@@ -0,0 +1,17 @@
+//
+//  DribbleAlanciaApp.swift
+//  Shared
+//
+//  Created by AJ Picard on 10/30/20.
+//
+
+import SwiftUI
+
+@main
+struct DribbleAlanciaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
